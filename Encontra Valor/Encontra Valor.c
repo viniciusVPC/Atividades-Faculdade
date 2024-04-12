5af7171d09d68e3ebb3bc13fe5c7372fccfd847a
profslpa <profslpa@ulife.com.br>
2024-04-12 11:18:01 -0300

    Adicionada função de Busca Binária

--- Encontra Valor/Encontra Valor.c
+++ Encontra Valor/Encontra Valor.c
@@ -13,6 +13,21 @@ void imprimeVetor(int *vetor, int tamanho){
     for(int i = 0; i < tamanho; i++) printf("%d | ", vetor[i]);
 }
 
+int encontraBinario(int valor, int *vetor, int tamanho){
+    if(vetor[tamanho-1] < valor || valor < vetor[0]) return -1;
+
+    int esquerda = 0;
+    int direita = tamanho-1;
+
+    while(esquerda<=direita){
+        int meio = (esquerda+direita)/2;
+        if(vetor[meio] < valor) esquerda = meio+1;
+        if(vetor[meio] > valor) direita = meio-1;
+        if(vetor[meio] == valor) return meio;
+    }
+    return -1;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
@@ -22,7 +37,7 @@ int main()
     int valor;
     printf("\n\nDigite o número a procurar: ");
     if(scanf("%d", &valor)){
-        printf("\nO índice do valor %d é %d\n\n", valor, encontra(valor, vetor));
+        printf("\nO índice do valor %d é %d\n\n", valor, encontraBinario(valor, vetor, 11));
     }
     else printf("\nIsso não é um número, mané\n\n");
     return 0;
