55bb34ca3367afec6c20393258a7690d593948de
profslpa <profslpa@ulife.com.br>
2024-05-24 10:42:58 -0300

    Adicionei quicksort

---
+++ QuickSort/QuickSort.c
@@ -0,0 +1,32 @@
+//Arquivo C
+#include<stdio.h>
+
+int separar(int p, int r, int v[]){
+int q = v[r-1];
+int i = p-1;
+for(int j = p; j < r; j++){
+    if(v[j] <= q){
+        int temp = v[++i];
+        v[i] = v[j];
+        v[j] = temp;
+    }
+}
+return i;
+}
+
+void quickSort(int p, int r, int v[]){
+    if(p<r-1){
+        int q = separar(p, r, v);
+        quickSort(p, q, v);
+        quickSort(q+1, r, v);
+    }
+}
+
+int main(){
+    int v[] = {99, 33, 55, 77, 11, 22, 88, 66, 33, 44};
+    quickSort(0, 10, v);
+    for(int i = 0; i<10; i++){
+        printf("%d , ", v[i]);
+    }
+return 0;
+}
